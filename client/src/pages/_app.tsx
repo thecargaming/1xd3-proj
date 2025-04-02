426e1364954b216a2e142a8cded3b3e0b26160d2
patrick <63032462+cryollyne@users.noreply.github.com>
2025-04-02 16:16:42 -0400

    added scss

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app";
+
+import '../global.scss';
+
+export default function App({Component, pageProps}: AppProps<any>) {
+    return <Component {...pageProps} />;
+}
