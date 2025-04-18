cba724ef8b7e5da31feda460eeb6fa0e716c2d6a
patrick <63032462+cryollyne@users.noreply.github.com>
2025-04-18 16:39:49 -0400

    made account related pages redirect upon visiting when not page is not applicable

--- client/src/pages/account.tsx
+++ client/src/pages/account.tsx
@@ -10,7 +10,7 @@ export default function Profile() {
     const router = useRouter();
     if (accountInfo == null) {
         setTimeout(()=>{
-            router.push('/');
+            router.push('/login');
         }, 0);
         return;
     };
