24cc89d8d272923d7e380c095d29b678526a3e31
Patrick <63032462+cryollyne@users.noreply.github.com>
2025-04-23 13:53:11 -0400

    added styling for account infomation container

--- client/src/pages/account.tsx
+++ client/src/pages/account.tsx
@@ -1,11 +1,15 @@
+import HLayout from "components/h_layout";
 import Layout from "components/layout";
 import { RegisterCompany } from "components/register_company";
 import { RegisterRepresentative } from "components/register_representative";
+import RoundContainer from "components/round_container";
+import VLayout from "components/v_layout";
 import { AccountInfoContext } from "context";
 import { basePrefix } from "net_utils";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import { BiLogOut } from "react-icons/bi";
+import styles from './account.module.scss';
 
 export default function Profile() {
     const [accountInfo, updateAccountInfo] = useContext(AccountInfoContext);
@@ -28,11 +32,19 @@ export default function Profile() {
 
     return (
         <Layout>
-        <h1>{accountInfo.firstName} {accountInfo.lastName}</h1>
-        <h2>{accountInfo.email}</h2>
-        <button onClick={logout}>Log out <BiLogOut /></button>
-        <RegisterCompany />
-        <RegisterRepresentative />
+        <VLayout>
+            <RoundContainer>
+                <VLayout>
+                    <h1>{accountInfo.firstName} {accountInfo.lastName}</h1>
+                    <h2>{accountInfo.email}</h2>
+                    <button onClick={logout} className={styles.button}>Log out <BiLogOut /></button>
+                </VLayout>
+            </RoundContainer>
+            <HLayout>
+                <RegisterCompany />
+                <RegisterRepresentative />
+            </HLayout>
+        </VLayout>
         </Layout>
     )
 }
