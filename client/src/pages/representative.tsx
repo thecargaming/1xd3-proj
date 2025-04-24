f32179b50abd9f32d8e68f75009564afd905b5e3
Patrick <63032462+cryollyne@users.noreply.github.com>
2025-04-23 22:37:15 -0400

    added display for availability

--- client/src/pages/representative.tsx
+++ client/src/pages/representative.tsx
@@ -7,17 +7,40 @@ import { useRouter } from "next/router";
 import { createRef, useContext, useEffect, useState } from "react";
 import styles from './representative.module.scss';
 import HLayout from "components/h_layout";
+import { WeekCalendar } from "components/calendar";
 
-type Company ={
+type Company = {
     representative_id: number
     company_name: string 
-} 
+}
+
+type Availability = {
+    day_of_week: number,
+    start_time: Date,
+    end_time: Date,
+    company_name: string,
+    id: number,
+};
 
 export default function Representative() {
     const [accountInfo, _] = useContext(AccountInfoContext);
     const router = useRouter();
     const [companies, setCompanies] = useState<Company[]>([]);
+    const [availability, setAvailability] = useState<Availability[]>([]);
     const selectedCompanyElement = createRef<HTMLSelectElement>();
+
+    const updateAvailability = async () => {
+        let res = await fetch(basePrefix('/api/representative/get_availability.php'));
+        if (!res.ok) {console.log(await res.json()); return;}
+        let availability_raw: any[] = await res.json();
+        setAvailability(availability_raw.map((a) => ({
+            day_of_week: a.day_of_week,
+            company_name: a.company_name,
+            start_time: new Date(`1970-01-${4+a.day_of_week} ${a.start_time}`),
+            end_time: new Date(`1970-01-${4+a.day_of_week} ${a.end_time}`),
+            id: a.id,
+        })));
+    };
 
     useEffect(()=>{(async() => {
         if (accountInfo == null) {
@@ -35,6 +58,7 @@ export default function Representative() {
             if (selectedCompanyElement.current)
                 selectedCompanyElement.current.value = "0";
         }
+        await updateAvailability();
     })()}, []);
 
     const [selectedCompany, setSelectedCompany] = useState(null as Company | null);
@@ -58,9 +82,26 @@ export default function Representative() {
                     </VLayout>
                 </RoundContainer>
                 {selectedCompany ? (
-                    <RoundContainer>
-                        <h1>{selectedCompany.company_name}</h1>
-                    </RoundContainer>
+                    <VLayout>
+                        <RoundContainer>
+                            <h1>{selectedCompany.company_name}</h1>
+                        </RoundContainer>
+                        <HLayout>
+                            <RoundContainer>
+                                <h1>Insert availability slow</h1>
+                            </RoundContainer>
+                            <RoundContainer>
+                                <h1>Delete availability slow</h1>
+                            </RoundContainer>
+                        </HLayout>
+                        <WeekCalendar events={availability.map((a) => ({
+                            begin: a.start_time,
+                            end: a.end_time,
+                            type: a.company_name === selectedCompany.company_name ? "representative" : "available",
+                            name: a.company_name,
+                            description: `Meeting id: ${a.id}`
+                        }))} />
+                    </VLayout>
                 ) : <></>}
             </VLayout>
         </Layout>
